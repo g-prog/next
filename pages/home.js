16aae339d811348126328f8c04fb0da8165d1328
g-prog <alaobukky2@gmail.com>
2021-09-10 04:54:18 -0700

    cleaned up home.js

--- pages/home.js
+++ pages/home.js
@@ -1,207 +1,27 @@
 import React from 'react'
-import Image from "next/image";
 import styles from "../styles/buky.module.css";
 
 function home() {
     return (
         <div>
             <div className="bg-beauty bg-cover min-h-full pt-2 px-2 md:px-20 ">
-            <div className="my-80 mx-44 flex h-screen">
-                
-                <div className={styles.bubble}>
-                   
-
-                 <img src="bubble.png"/>
-                 <img src="bubble.png"/>
-                 <img src="bubble.png"/>
-                 <img src="bubble.png"/>
-                 <img src="bubble.png"/>
-                 <img src="bubble.png"/>
-                 <img src="bubble.png"/>
-                 <img src="bubble.png"/>
-                 <img src="bubble.png"/>
-                 <img src="bubble.png"/>
-
-
-                </div>
-                
-            </div>
-            </div>
-            <div>
-
-            </div>
-             
-             
-
-                <div className="flex h-full">
-                <div className="mx-auto  flex flex-row">
-                <div className="bg-gray-500 p-10">
-                    <Image
-                    src="/female2.jpg"
-                    width={250}
-                    height={300}
-                    alt="white background"
-                    />
-
-
-    
-                </div>
-               
-                
-            </div>
-
-                
-                
-
+                <div className="my-80 mx-44 flex h-screen">
                     
-
+                    <div className={styles.bubble}>
+                        <img src="bubble.png"/>
+                        <img src="bubble.png"/>
+                        <img src="bubble.png"/>
+                        <img src="bubble.png"/>
+                        <img src="bubble.png"/>
+                        <img src="bubble.png"/>
+                        <img src="bubble.png"/>
+                        <img src="bubble.png"/>
+                        <img src="bubble.png"/>
+                        <img src="bubble.png"/>
+                    </div>
                     
-                  
-
-            </div>
-
-
-            <div className="flex h-full">
-                <div className="mx-auto  flex flex-row">
-                <div className="bg-gray-500 p-10">
-                    <Image
-                    src="/female2.jpg"
-                    width={250}
-                    height={300}
-                    alt="white background"
-                    />
-
-
-    
                 </div>
-                <div className="p-12">
-                    <p>
-                        blah blah blah blah blah blah blah have you any wool, yes sir yes dirnjfifopfkfmfhfhfhfufkf
-                    </p>
-                </div>
-
-                
             </div>
-
-                
-                
-
-                    
-
-                    
-                  
-
-            </div>--
-
-
-            <div className="flex h-full">
-                <div className="mx-auto  flex flex-row">
-                <div className="bg-gray-500 p-10">
-                    <Image
-                    src="/female2.jpg"
-                    width={250}
-                    height={300}
-                    alt="white background"
-                    />
-
-
-    
-                </div>
-                <div className="p-12">
-                    <p>
-                        blah blah blah blah blah blah blah have you any wool, yes sir yes dirnjfifopfkfmfhfhfhfufkf
-                    </p>
-                </div>
-
-                
-            </div>
-
-                
-                
-
-                    
-
-                    
-                  
-
-            </div>
-                
-            
-       
-            <div className="flex h-full">
-                <div className="mx-auto  flex flex-row">
-                <div className="bg-gray-500 p-10">
-                    <Image
-                    src="/female2.jpg"
-                    width={250}
-                    height={300}
-                    alt="white background"
-                    />
-
-
-    
-                </div>
-                <div className="p-12">
-                    <p>
-                        blah blah blah blah blah blah blah have you any wool, yes sir yes dirnjfifopfkfmfhfhfhfufkf
-                    </p>
-                </div>
-
-                
-            </div>
-
-                
-                
-
-                    
-
-                    
-                  
-
-            </div>
-                
-            
-            <div className="flex h-full">
-                <div className="mx-auto  flex flex-row">
-                <div className="bg-gray-500 p-10">
-                    <Image
-                    src="/female2.jpg"
-                    width={250}
-                    height={300}
-                    alt="white background"
-                    />
-
-
-    
-                </div>
-                <div className="p-12">
-                    <p>
-                        blah blah blah blah blah blah blah have you any wool, yes sir yes dirnjfifopfkfmfhfhfhfufkf
-                    </p>
-                </div>
-
-                
-            </div>
-
-                
-                
-
-                    
-
-                    
-                  
-
-            </div>
-                
-            
-       
-
-                
-            
-       
-                
-            
-       
             
 
         </div>
